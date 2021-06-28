238d134bfea45a87d18d3102404416aaf5f5892f
Heather Storseth <heatherstorseth3@gmail.com>
2021-06-28 15:19:43 -0400

    resume opens in new tab

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,16 +7,16 @@ function Footer() {
         <section id="contact-info" className="contact-info" alt="contact information">
           <h2>Contact Info</h2>
           {/* target blank alternative for react */}
-          <a href="mailto: heatherstorseth3@gmail.com" target="_blank">
+          <a href="mailto: heatherstorseth3@gmail.com" target={"_blank"}>
             <i className="fas fa-envelope"></i>
           </a>
-          <a href="./assets/resume2.0.pdf" target="_blank">
+          <a href="https://docs.google.com/document/d/1BlfVQj222OgecC24HAn5Wlj2G8MJMamOeUhV4rkn0eA/edit?usp=sharing" target={"_blank"}>
             <i className="fas fa-file"></i>
           </a>
-          <a href="www.linkedin.com/in/heather-storseth-5571501b9" target="_blank">
+          <a href="www.linkedin.com/in/heather-storseth-5571501b9" target={"_blank"}>
             <i className="fab fa-linkedin"></i>
           </a>
-          <a href="https://github.com/hstor3" target="_blank">
+          <a href="https://github.com/hstor3" target={"_blank"}>
             <i className="fab fa-github-square"></i>
           </a>
           <a href="tel: +1-509-901-1565">
